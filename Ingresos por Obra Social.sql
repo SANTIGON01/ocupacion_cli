4033a1261de6581631b3233abeee165be3f8329b
Fran Chiappetta <145611227+chiappettafran@users.noreply.github.com>
2024-11-11 20:41:26 -0300

    Add files via upload

---
+++ Ingresos por Obra Social.sql
@@ -0,0 +1,16 @@
+DECLARE @FechaInicio DATETIME = '2024-05-01';
+DECLARE @FechaFin DATETIME = '2024-05-31';
+
+SELECT 
+    O.OSRazonSocial as 'Obra Social', 
+    COUNT(I.INNumInt) AS Internaciones
+FROM 
+    INTERNAD I
+JOIN 
+    OBRASOCIAL O ON I.INObraSocial = O.OSCodigo
+WHERE 
+    I.INFechaIngreso >= @FechaInicio and I.INFechaIngreso <= @FechaFin
+GROUP BY 
+    O.OSRazonSocial
+ORDER BY 
+    o.OSRazonSocial;
